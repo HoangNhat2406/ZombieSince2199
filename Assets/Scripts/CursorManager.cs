afcaba4a30d5f1ce485f813e4d2eccc5905495c4
TheBigMouse-Fstack <khanhnqse19@gamil.com>
2026-03-23 21:08:08 +0700

    Energy Enemy

--- Assets/Scripts/CursorManager.cs
+++ Assets/Scripts/CursorManager.cs
@@ -2,35 +2,47 @@ using UnityEngine;
 
 public class CursorManager : MonoBehaviour
 {
-    [SerializeField] private Texture2D cursorNomal;
+    [SerializeField] private Texture2D cursorNormal; // Sửa lỗi chính tả Nomal -> Normal
     [SerializeField] private Texture2D cursorShoot;
     [SerializeField] private Texture2D cursorReload;
 
-    private Vector2 hotspot = new Vector2(16, 48);
+    // Hotspot nên là tâm của đầu ruồi súng (Crosshair)
+    // Nếu ảnh là 32x32, tâm sẽ là (16, 16)
+    [SerializeField] private Vector2 hotspot = new Vector2(16, 16);
 
     void Start()
     {
-        Cursor.SetCursor(cursorNomal, hotspot, CursorMode.Auto);
+        SetCustomCursor(cursorNormal);
     }
 
     void Update()
     {
+        // Sử dụng GetMouseButton thay vì Down/Up nếu bạn muốn logic chặt chẽ hơn
         if (Input.GetMouseButtonDown(0))
         {
-            Cursor.SetCursor(cursorShoot, hotspot, CursorMode.Auto);
+            SetCustomCursor(cursorShoot);
         }
         else if (Input.GetMouseButtonUp(0))
         {
-            Cursor.SetCursor(cursorNomal, hotspot, CursorMode.Auto);
+            SetCustomCursor(cursorNormal);
         }
 
         if (Input.GetMouseButtonDown(1))
         {
-            Cursor.SetCursor(cursorReload, hotspot, CursorMode.Auto);
+            SetCustomCursor(cursorReload);
         }
         else if (Input.GetMouseButtonUp(1))
         {
-            Cursor.SetCursor(cursorNomal, hotspot, CursorMode.Auto);
+            SetCustomCursor(cursorNormal);
+        }
+    }
+
+    private void SetCustomCursor(Texture2D tex)
+    {
+        if (tex != null)
+        {
+            // Dùng CursorMode.Auto hoặc ForceSoftware nếu trên Linux gặp lỗi hiển thị
+            Cursor.SetCursor(tex, hotspot, CursorMode.Auto);
         }
     }
 }
